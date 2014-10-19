dbc377f5f27599bca298dffac34f27017eec44ea
root <root@ip-172-31-20-210.ec2.internal>
2014-10-19 06:42:59 +0000

    updating drill install path

--- docker/drill-base/install-drill.sh
+++ docker/drill-base/install-drill.sh
@@ -4,7 +4,7 @@ mkdir -p /opt/drill
 
 cd /tmp
 wget http://apache.tradebit.com/pub/incubator/drill/drill-0.5.0-incubating/apache-drill-0.5.0-incubating.tar.gz
-tar -xvzf apache-drill-0.5.0-incubating.tar.gz -C /opt/drill
+tar -xvzf apache-drill-0.5.0-incubating.tar.gz -C /opt/drill --strip-components=1
 rm /tmp/apache-drill-0.5.0-incubating.tar.gz
 
-cd /opt/drill/apache-drill-0.5.0-incubating
+cd /opt/drill
