9b3cc953068ab6b3ab4f880e56f28324b0fe815b
Ubuntu <ubuntu@ip-172-31-22-197.ec2.internal>
2014-10-19 07:43:18 +0000

    wiring up zookeeper

--- docker/drill-base/install.sh
+++ docker/drill-base/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 mkdir -p /opt/drill
-
 cd /tmp
+
 wget http://apache.tradebit.com/pub/incubator/drill/drill-0.5.0-incubating/apache-drill-0.5.0-incubating.tar.gz
 tar -xvzf apache-drill-0.5.0-incubating.tar.gz -C /opt/drill --strip-components=1
 rm /tmp/apache-drill-0.5.0-incubating.tar.gz
 
-cd /opt/drill
+sed -i "s/ZOO_HOST/$LOCAL_HOST/g" /tmp/drill-override.conf
+cp /tmp/drill-override.conf /opt/drill/conf/drill-override.conf
+rm /tmp/drill-override.conf
--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "LOCAL_HOST=$(hostname)" > $DIR/drill-base/local-data
+
 docker build -t thundermoose/drill-zookeeper ./drill-zookeeper
 docker build -t thundermoose/drill-base ./drill-base
 docker build -t thundermoose/drill-server ./drill-server
